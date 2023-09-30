baa224855c830e3cd7afcaf6e52e64f29aae9bc3
David <davidmfc1092@hotmail.com>
2023-09-30 13:14:54 -0500

    testimonio

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent  implements OnInit {
   banner:any
   iconWhatsaap:any
+  banner2:any
+  photo:any
   cards:any = [
      {
       name:'Lucha 1',
@@ -86,22 +88,26 @@ export class HomeComponent  implements OnInit {
         'tema'
       ]
     },
+   
     {
       name:'Lucha 5',
       descripcion:'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido',
-      imagen:'https://img.freepik.com/foto-gratis/cadena-acero-entrelazada-es-simbolo-fuerza-generado-ia_188544-38345.jpg?w=1380&t=st=1695999012~exp=1695999612~hmac=1432d8258bc4209ccb56264b8e0900344c515638c183443304bc68290f47f1fd',
+      imagen:'https://img.freepik.com/foto-gratis/nino-corte-pelo-rosa-palabra-arcoiris-frente_1340-41699.jpg?w=1380&t=st=1695937387~exp=1695937987~hmac=8378a1ba65fbc47ef280be4a923ab3c55e0a1d1106f4ad015d36fb0b1c2f47f4',
       temas:[
         'tema',
         'tema',
         'tema',
         'tema'
       ]
-    }
+    },
   ]
  
   ngOnInit(): void {
-    this.banner = 'assets/img/11.webp'
+    this.banner = 'assets/img/cruz-cristiana-naturaleza.jpg'
     this.iconWhatsaap = 'assets/img/icon_whatsaap.png' 
+    this.banner2 = 'assets/img/never.jpg' 
+    this.photo = 'assets/img/testimonio.jpg' 
+  
   }
 
 }
